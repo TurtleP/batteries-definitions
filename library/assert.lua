3495214a37384846506a06291f56e9ed0c802771
TurtleP <jpostelnek@outlook.com>
2023-03-05 20:01:03 -0500

    add functional, mark necessary assert params optional

--- library/assert.lua
+++ library/assert.lua
@@ -5,8 +5,8 @@ local assert = {}
 
 ---Asserts a value is not nil.
 ---@param value any Value to check
----@param message string Error message
----@param stack_level integer Error message stack level (default `0`)
+---@param message? string Error details message (default `""`)
+---@param stack_level? integer Error message stack level (default `0`)
 ---@return any value Value that was checked, for chaining
 function assert:some(value, message, stack_level)
 end
@@ -14,8 +14,8 @@ end
 ---Asserts two values are equal.
 ---@param a any First value
 ---@param b any Second value
----@param message string Error message
----@param stack_level integer Error message stack level (default `0`)
+---@param message? string Error details message (default `""`)
+---@param stack_level? integer Error message stack level (default `0`)
 ---@return any a Value that was checked, for chaining
 function assert:equal(a, b, message, stack_level)
 end
@@ -23,8 +23,8 @@ end
 ---Asserts two values are **not** equal.
 ---@param a any First value
 ---@param b any Second value
----@param message string Error message
----@param stack_level integer Error message stack level (default `0`)
+---@param message? string Error details message (default `""`)
+---@param stack_level? integer Error message stack level (default `0`)
 ---@return any a Value that was checked, for chaining
 function assert:not_equal(a, b, message, stack_level)
 end
@@ -32,8 +32,8 @@ end
 ---Asserts that @value is of type @type.
 ---@param value any Value to check
 ---@param type any Type to check on @value
----@param message string Error message
----@param stack_level integer Error message stack level (default `0`)
+---@param message? string Error details message (default `""`)
+---@param stack_level? integer Error message stack level (default `0`)
 ---@return any value Value that was checked, for chaining
 function assert:type(value, type, message, stack_level)
 end
@@ -41,8 +41,8 @@ end
 ---Asserts that @value is of type @type or `nil`.
 ---@param value any Value to check
 ---@param type any Type to check on @value
----@param message string Error message
----@param stack_level integer Error message stack level (default `0`)
+---@param message? string Error details message (default `""`)
+---@param stack_level? integer Error message stack level (default `0`)
 ---@return any value Value that was checked, for chaining
 function assert:type_or_nil(value, type, message, stack_level)
 end
---
+++ library/functional.lua
@@ -0,0 +1,338 @@
+---@meta
+
+---@class functional
+local functional = {}
+
+---Returns the identity of a value.
+---@param value any Value to get the identity of
+---@return any value Value that was identitied
+function functional.identity(value)
+end
+
+---Simple sequential iteration. \
+---The @callback can return non-nil to break the loop, returning its value. \
+---Otherwise, returns the table for chaining.
+---@param table table Table to iterate
+---@param callback function Function to call on each element
+---@return any result See comments.
+function functional.foreach(table, callback)
+end
+
+---Perform a left-to-right reduction on @table using @callback with @seed as an initial value. \
+---This is performed iteratively, so there is no stack smashing. \
+---Example: `reduce({1, 2, 3}, 0, f) -> f(f(f(0, 1), 2), 3)`
+---@param table table Table to iterate
+---@param seed any Value to capture
+---@param callback function Callback declared as `f(@seed, table_element, index)
+---@return any seed See @seed
+function functional.reduce(table, seed, callback)
+end
+
+---Maps a sequence without modifying the original data. \
+---This automatically drops any `nil`s to simultaneously map and filter.
+---@param table table Table to iterate
+---@param callback function Callback declared as `f(table_element, index)`
+---@return table mapped The newly mapped sequence
+function functional.map(table, callback)
+end
+
+---Maps a sequence in-place, modifying it. \
+---This automatically drops any `nil`s to simultaneously map and filter.
+---@param table table Table to iterate
+---@param callback function Callback declared as `f(table_element, index)`
+---@return table mapped The newly mapped sequence
+function functional.map_inplace(table, callback)
+end
+
+---Alias for map_inplace
+functional.remap = functional.map_inplace
+
+---Maps a sequence. \
+---This automatically drops any `nil`s to simultaneously map and filter. \
+---Example:
+---```
+---local a = { ["a"] = 1,["b"] = 4 }
+---local b = { ["a"] = 2,["b"] = 5 }
+---local c = { ["a"] = 3,["b"] = 6 }
+---
+---local result = async.map_field({ a, b, c }, "b")
+---  -> result: {4, 5, 6}
+---```
+---@param table table Table of tables to iterate
+---@param key string Key to map to
+---@return table mapped The newly mapped sequence
+function functional.map_field(table, key)
+end
+
+---Maps a sequence by a method call. \
+---This automatically drops any `nil`s to simultaneously map and filter.
+---Example:
+---```
+---local a = {}
+---function a:position()
+---    return { 1, 2 }
+---end
+---
+---local b = {}
+---function b:position()
+---    return { 3, 4 }
+---end
+---
+---function c(element, one, two)
+---    return one * two
+---end
+---
+---local seq_a = functional.map_call({ a, b }, "position")
+---  -> { { 1, 2 }, { 3, 4 }}
+---local seq_b = functional.map_call({ a, b }, c, 1, 2)
+---  -> { 2, 2 }
+---```
+---@param table table Table to iterate
+---@param method string|function Method to map by
+---@vararg any Arguments to pass
+---@return table result The newly mapped sequence
+function functional.map_call(table, method, ...)
+end
+
+---Maps a sequence into a new index space. \
+---The callback function may return an index where the value will be stored in the result. \
+---If no index (or `nil`) is provided, it will insert as normal.
+---@param table table Table to iterate
+---@param callback function Callback declared as `f(table_element, index)`
+---@return table result The newly mapped sequence
+function functional.splat(table, callback)
+end
+
+---Returns a sequence containing items where `callback(value)` returns truthy. \
+---@param table table Table to iterate
+---@param callback function Callback declared as `f(table_element, index)`
+---@return table result The new sequence
+function functional.filter(table, callback)
+end
+
+---Returns a sequence containing items where `callback(value)` returns truthy. \
+---This will perform operations in-place, modifying the original data.
+---@param table table Table to iterate
+---@param callback function Callback declared as `f(table_element, index)`
+---@return table result The original sequence, modified
+function functional.filter_inplace(table, callback)
+end
+
+---Returns a sequence containing items where `callback(value)` returns falsey. \
+---`nil` results are included to be the exact complement of filter; use `functional.partition` if you need both.
+---@param table table Table to iterate
+---@param callback function Callback declared as `f(table_element, index)`
+function functional.remove_if(table, callback)
+end
+
+---Paritions a sequence into two, based on filter criteria.
+---@param table table Table to iterate
+---@param callback function Callback declared as `f(table_element, index)`
+---@return table sequence_one First partition
+---@return table sequence_two Second partition
+function functional.partition(table, callback)
+end
+
+---Returns a sequence where elements in the table are grouped into sequential tables by the result of the callback on each element. \
+---This is more general than parition, but knowledge of groups ahead of time. \
+---Use numeric grouping and pre-seed if you want to avoid pairs!
+---@param table table Table to iterate
+---@param callback function Callback declared as `f(table_element, index)`
+---@return table result The newly mapped group data
+function functional.group_by(table, callback)
+end
+
+---Zips two sequences together into a new table, based on a callback. \
+---Iteration is limited by min(#table_one, #table_two). \
+---`nil` results are ignored.
+---@param table_one table The first table to zip
+---@param table_two table The second table to zip
+---@param callback function Callback declared as `f(table_one_element, table_two_element, index)`
+---@return table result The newly zipped data sequence
+function functional.zip(table_one, table_two, callback)
+end
+
+-------------------
+---Specialized Maps
+-------------------
+
+---Maps a sequence where the callback generaly results into a sequence, appending data into one big sequence.
+---This automatically drops any `nil`s to simultaneously map and filter.
+---@param table table Table to iterate
+---@param callback function Callback declared as `f(table_element, index)`
+---@return table result The new sequence
+function functional.stitch(table, callback)
+end
+
+---Alias
+functional.map_stitch = functional.stitch
+
+---Maps a sequence by cycling through the input data. \
+---This is useful for inter-dependent data. \
+---This automatically drops any `nil`s to simultaneously map and filter. \
+---Example:
+---```
+---local sequence = functional.cycle({ 1, 2, 3 }, function(a, b)
+---    print(a, b)
+---end)
+---  -> 1  2
+---  2. 2  3
+---  3. 3  1
+---```
+---@param table table Table to iterate
+---@param callback function Callback declared as `f(table_element, table_element)`
+---@return table result The new sequence
+function functional.cycle(table, callback)
+end
+
+---Alias
+functional.map_cycle = functional.cycle
+
+---Maps a sequence by chaining through the input data. \
+---This is useful for inter-dependent data. \
+---This automatically drops any `nil`s to simultaneously map and filter. \
+---Example:
+---```
+---local sequence = functional.cycle({ 1, 2, 3 }, function(a, b)
+---    print(a, b)
+---end)
+---  -> 1  2
+---  2. 2  3
+---```
+---@param table table Table to iterate
+---@param callback function Callback declared as `f(table_element_previous, table_element)`
+---@return table result The new sequence
+function functional.chain(table, callback)
+end
+
+---Alias
+functional.map_chain = functional.chain
+
+-------------------
+--- Data Generation
+-------------------
+
+---Generate data into a table. \
+---This automatically drops any `nil`s to simultaneously map and filter.
+---@param count integer Total number of items to generate
+---@param callback function Callback declared as `f(index)`
+---@return table result The newly generated sequence
+function functional.generate(count, callback)
+end
+
+---Generate data into via a two-dimensional iteration. \
+---If a two-dimensional table is required, nest one-dimensional calls. \
+---**Note:** this returns a one-dimensional table.
+---@param width integer The x-axis size
+---@param height integer the y-axis size
+---@param callback function Callback declared as `f(x, y)`
+---@return table result The newly generated sequence
+function functional.generate_2d(width, height, callback)
+end
+
+---------------------------------
+--- Common queries and reductions
+---------------------------------
+
+---Return whether any table element matches the query.
+---@param table table Table to iterate
+---@param callback function Callback declared as `f(table_element, index)`
+---@return boolean matching
+function functional.any(table, callback)
+end
+
+---Return whether none of the table elements match the query.
+---@param table table Table to iterate
+---@param callback function Callback declared as `f(table_element, index)`
+---@return boolean none_matching
+function functional.none(table, callback)
+end
+
+---Return whether all the table elements match the query.
+---@param table table Table to iterate
+---@param callback function Callback declared as `f(table_element, index)`
+---@return boolean matching
+function functional.all(table, callback)
+end
+
+---Return whether the table contains an element.
+---@param table table Table to iterate
+---@param value any The value to check for
+---@return boolean found
+function functional.contains(table, value)
+end
+
+---Counts the elements of the table that match the query.
+---@param table table Table to iterate
+---@param callback function Callback declared as `f(table_element, index)`
+---@return integer count
+function functional.count(table, callback)
+end
+
+---Return the numeric sum of all elements in the table.
+---@param table table The table to iterate
+---@return number sum
+function functional.sum(table)
+end
+
+---Return the numeric mean of all elements in the table.
+---@param table table The table to iterate
+---@return number mean
+function functional.mean(table)
+end
+
+---Return the minimum and maximum of the table in one pass. \
+---This will return zero for both if the table is empty.
+---@param table table The table to iterate
+---@return number min
+---@return number max
+function functional.minmax(table)
+end
+
+---Return the minimum element of the table.
+---This will return zero if the table is empty.
+---@param table table
+---@return number min
+function functional.min(table)
+end
+
+---Return the maximum element of the table.
+---This will return zero if the table is empty.
+---@param table table
+---@return number max
+function functional.max(table)
+end
+
+---Return the element that results in the lowest numeric value.
+---@param table table The table to iterate
+---@param callback function Callback declared as `f(table_element, index)`
+---@return number min
+function functional.find_min(table, callback)
+end
+
+---Return the element that results in the greatest numeric value.
+---@param table table The table to iterate
+---@param callback function Callback declared as `f(table_element, index)`
+---@return number max
+function functional.find_max(table, callback)
+end
+
+---Alias
+functional.find_best = functional.find_max
+
+---Return the element that results in the value nearest to the target value. \
+---**Note:** This function is not well-optimized.
+---@param table table The table to iterate
+---@param callback function Callback declared as `f(table_element, index)`
+---@param target number The target reference number
+---@return number nearest
+function functional.find_nearest(table, callback, target)
+end
+
+---Return the first element that results in a truthy value.
+---@param table table Table to iterate
+---@param callback function Callback declared as `f(table_element)`
+function functional.find_match(table, callback)
+end
+
+return functional
